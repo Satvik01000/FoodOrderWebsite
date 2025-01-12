ce5e745ba4653f51799789d498392058f4fcae88
Satvik Mishra <satvik0100@Satviks-MacBook-Air-2.local>
2025-01-12 14:12:16 +0530

    Routing added

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+const About = ()=>{
+  return(
+      <div className="about-container">
+          <h1 className="about-heading">
+              React Learning
+              <h2>
+                  This is food delivery website to learn React
+              </h2>
+          </h1>
+      </div>
+  );
+};
+
+export default About;
--- src/scripts.js
+++ src/scripts.js
@@ -2,15 +2,43 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Body from "./components/Body";
 import Header from "./components/Header";
+import {createBrowserRouter, RouterProvider, Outlet} from "react-router";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import RestaurantMenu from "./components/RestaurantMenu";
+
 const rootElement = ReactDOM.createRoot(document.getElementById("root"));
 
-const AppLayout=()=>{
+const AppLayout = () => {
     return (
         <div className="App">
-            <Header/>
-            <Body/>
+            <Header />
+            <Outlet />
         </div>
     );
 };
 
-rootElement.render(<AppLayout/>);
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout />,
+        errorElement:<Error/>,
+        children:[
+            {
+              path:'/',
+              element:<Body/>
+            },
+            {
+                path: "/about",
+                element: <About />
+            },
+            {
+                path:"/contact-us",
+                element:<Contact/>
+            }
+        ]
+    },
+]);
+
+rootElement.render(<RouterProvider router={appRouter} />);
