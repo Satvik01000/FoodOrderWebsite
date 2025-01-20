4a43a4ea99928ec4b3bc83e088d9454020819e91
Satvik Mishra <satvik0100@Satviks-MacBook-Air-2.local>
2025-01-21 00:56:13 +0530

    Second Last Commit

--- src/scripts.js
+++ src/scripts.js
@@ -6,7 +6,6 @@ import {createBrowserRouter, RouterProvider, Outlet} from "react-router";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
-import RestaurantMenu from "./components/RestaurantMenu";
 
 const rootElement = ReactDOM.createRoot(document.getElementById("root"));
 
